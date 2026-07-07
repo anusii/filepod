ae6dba866da90efc505a3568294a60ca5fd13d01
Tony Chen <yang.chen2@anu.edu.au>
2026-07-07 13:51:55 +1000

    Update the path of clientId in lib/app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -62,7 +62,7 @@ class App extends StatelessWidget {
         logo: const AssetImage('assets/images/app_icon.png'),
         link: 'https://github.com/anusii/filepod',
         clientId:
-            'https://filepod.solidcommunity.au/client-profile.jsonld',
+            'https://solidcommunity.au/apps/filepod/client-profile.jsonld',
         redirectUris: [
           'https://filepod.solidcommunity.au/redirect.html',
           'com.togaware.filepod://redirect',
